57c29f2fac1d202735385dc6f755616028bb326c
brrrak <102214069+brrrak@users.noreply.github.com>
2022-12-01 21:29:13 +0300

    day1 simplified

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,42 +1,31 @@
 #include <fstream>
 #include <iostream>
-#include <queue>
 
-void getCalories(std::priority_queue<int>& calories, std::string input_name) {
-    int curCals{0};
-    std::ifstream finput(input_name);
+int main() {
+
+    int curCals{};
+    int b1{}, b2{}, b3{};
+
+    std::ifstream finput("input.txt");
     while (finput) {
         std::string line;
         getline(finput, line);
         if (line.length() == 0) {
-            if (curCals != 0) {
-                calories.push(curCals);
+            b3 = (curCals > b3) ? curCals : b3;
+            if (b3 > b2) {
+                std::swap(b2, b3);
+            }
+            if (b2 > b1) {
+                std::swap(b1, b2);
             }
             curCals = 0;
         } else {
             curCals += stoi(line);
         }
     }
-    if (curCals != 0) {
-        calories.push(curCals);
-        curCals = 0;
-    }
-}
 
-int main() {
-
-    std::priority_queue<int> calories;
-    getCalories(calories, "input.txt");
-
-    int sum{calories.top()};
-    calories.pop();
-    std::cout << "Part 1 Answer: " << sum << '\n';
-
-    for (int i{0}; i < 2; ++i) {
-        sum += calories.top();
-        calories.pop();
-    }
-    std::cout << "Part 2 Answer: " << sum << '\n';
+    std::cout << "Part 1 Answer: " << b1 << '\n';
+    std::cout << "Part 2 Answer: " << b1 + b2 + b3 << '\n';
 
     return 0;
 }
